ecbdabcf581ef961458e038b804706ef9fc3ccec
mibrahim234 <mohamm1417@gmail.com>
2022-01-08 21:18:14 -0500

    queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,19 +1,36 @@
+import { gql } from '@apollo/client';
 
 
 //login User
 
-gql`
-mutation routeLogin($email: String!, $password: String!) {
-    routeLogin(email: $email, password: $password){
-        token
-        user {
-            _id
-            username
-        }
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
-}`
+  }
+`;
 //add User
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 
 //Save Book
+export const SAVED_BOOKS = gql``
+
 
 //Remove Book
+export const REMOVE_BOOK = gql``
---
+++ client/src/utils/queries.js
@@ -0,0 +1,23 @@
+//utils; This folder will serve as a location to hold code and functionality that isn't necessarily React-based.
+
+// `queries.js`: This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks: {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}
+`
