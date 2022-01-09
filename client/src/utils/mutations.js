07e1b42d5c276b65d9476d9ac20b88399a616660
mibrahim234 <mohamm1417@gmail.com>
2022-01-09 14:15:36 -0500

    mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,6 @@ import { gql } from '@apollo/client';
 
 
 //login User
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
